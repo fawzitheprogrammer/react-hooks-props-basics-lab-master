682b852d6dfce2e5a81e00b02db863a060987e43
Fawzi Gharib <fawz11gharib@gmail.com>
2024-03-17 23:45:48 +0300

    Assingment Done

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Links } from "./Links";
+
+function About(props) {
+  const { bio, links } = props;
+
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      {bio && <p>{bio}</p>}
+      <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+      {bio ? (
+        <Links github={links.github} linkedin={links.linkedin} />
+      ) : null}
+    </div>
+  );
+}
+
+export default About;
